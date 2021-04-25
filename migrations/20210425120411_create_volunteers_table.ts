3eb0e7f549a4dcc15feaaacf029b67f9663fcb7e
Crepiks <sayazhan.onlassyn@mail.ru>
2021-04-25 12:34:36 +0600

    feat: implement create_volunteers_table migration

--- migrations/20210425120411_create_volunteers_table.ts
+++ migrations/20210425120411_create_volunteers_table.ts
@@ -1,5 +1,16 @@
 import * as Knex from 'knex';
 
-export async function up(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('volunteers', (table) => {
+    table.increments('id');
+    table.string('firstName', 255).notNullable();
+    table.string('lastName', 255).notNullable();
+    table.string('phoneNumber', 10).notNullable();
+    table.string('password', 255).notNullable();
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
+  });
+}
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('volunteers');
+}
