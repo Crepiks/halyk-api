a0ed3c0def0de3e7f701b314846bb4375390a870
Crepiks <sayazhan.onlassyn@mail.ru>
2021-04-25 13:55:13 +0600

    feat: implement create_admins_table migration

--- migrations/20210425123808_create_admins_table.ts
+++ migrations/20210425123808_create_admins_table.ts
@@ -1,16 +1,17 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('volunteers', (table) => {
+  return knex.schema.createTable('admins', (table) => {
     table.increments('id');
     table.string('firstName', 255).notNullable();
     table.string('lastName', 255).notNullable();
-    table.string('phoneNumber', 10).unique().notNullable();
+    table.string('email', 255).unique().notNullable();
+    table.timestamp('emailConfirmedAt').nullable().defaultTo(null);
     table.string('password', 255).notNullable();
     table.timestamp('createdAt').defaultTo(knex.fn.now());
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('volunteers');
+  return knex.schema.dropTable('admins');
 }
