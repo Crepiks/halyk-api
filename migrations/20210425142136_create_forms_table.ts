2e0216fb04875b063d1b80af9d637760a83f8259
Crepiks <sayazhan.onlassyn@mail.ru>
2021-04-25 14:39:02 +0600

    feat: implement create_forms_table migration

---
+++ migrations/20210425142136_create_forms_table.ts
@@ -0,0 +1,25 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('forms', (table) => {
+    table.increments('id');
+    table.string('fullName', 512).notNullable();
+    table.string('phone', 10).nullable();
+    table.string('address', 255).nullable();
+    table.integer('age').unsigned().nullable();
+    table.string('maritalStatus', 255).nullable();
+    table.integer('childrenNumber').unsigned().notNullable().defaultTo(0);
+    table.text('kept').nullable().defaultTo(null);
+    table.string('housing', 255).nullable();
+    table.string('ocupation', 512).nullable();
+    table.text('help').nullable();
+    table.text('helpAbsenceReason').nullable();
+    table.string('problems').nullable();
+    table.boolean('canLearn').nullable().defaultTo(false);
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('forms');
+}
